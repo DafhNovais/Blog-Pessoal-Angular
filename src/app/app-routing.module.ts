00935a04183ddf5d1d69f9a3d8675729ed90957e
Dafh.Novais <dafhnenovais@gmail.com>
2021-06-25 09:44:34 -0300

    Criando e redirecionando rotas.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path:'', redirectTo:'login', pathMatch:'full'},
+  {path:'login', component: LoginComponent},
+  {path:'cadastro', component: CadastroComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
