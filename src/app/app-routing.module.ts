54e2d16770d966513c48e0c3f72c4a19e302905c
Dafh.Novais <dafhnenovais@gmail.com>
2021-06-30 00:53:56 -0300

    Desenvolvendo consumo de API

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { InicioComponent } from './inicio/inicio.component';
 
 const routes: Routes = [
 
   {path:'', redirectTo:'login', pathMatch:'full'},
   {path:'login', component: LoginComponent},
-  {path:'cadastro', component: CadastroComponent}
+  {path:'cadastro', component: CadastroComponent},
+  {path:'inicio', component: InicioComponent}
   
 ];
 
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
+import { UserLogin } from '../model/UserLogin';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  userLogin: UserLogin = new UserLogin()
 
-  ngOnInit(): void {
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  entrar() {
+    this.auth.entrar(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.id = this.userLogin.id
+      environment.nome = this.userLogin.nome
+      environment.foto = this.userLogin.foto
+      environment.token = this.userLogin.token
+
+      console.log(environment.id)
+      console.log(environment.nome)
+      console.log(environment.foto)
+      console.log(environment.token)
+
+      this.router.navigate(['/inicio'])
+    }, erro => {
+      if(erro.status == 500) {
+        alert('Usuário ou senha incerretos!')
+      }
+    })
   }
 
 }
