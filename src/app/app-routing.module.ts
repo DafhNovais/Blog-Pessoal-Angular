29a091053b7e8b00e47426159eec99786870eb26
Dafh.Novais <dafhnenovais@gmail.com>
2021-07-08 10:43:37 -0300

    Implementação de edição de usuário

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { TemaEditComponent } from './edit/tema-edit/tema-edit.component';
 import { TemaDeleteComponent } from './delete/tema-delete/tema-delete.component';
 import { PostagemEditComponent } from './edit/postagem-edit/postagem-edit.component';
 import { PostagemDeleteComponent } from './delete/postagem-delete/postagem-delete.component';
+import { UserEditComponent } from './edit/user-edit/user-edit.component';
 
 const routes: Routes = [
 
@@ -22,7 +23,9 @@ const routes: Routes = [
   {path: 'tema-delete/:id', component: TemaDeleteComponent},
 
   {path: 'postagem-edit/:id', component: PostagemEditComponent},
-  {path: 'postagem-delete/:id', component: PostagemDeleteComponent}
+  {path: 'postagem-delete/:id', component: PostagemDeleteComponent},
+
+  {path: 'user-edit/:id', component: UserEditComponent}
   
 ];
 
---
+++ src/app/edit/user-edit/user-edit.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { User } from 'src/app/model/User';
+import { AuthService } from 'src/app/service/auth.service';
+import { PostagemService } from 'src/app/service/postagem.service';
+import { environment } from 'src/environments/environment.prod';
+
+@Component({
+  selector: 'app-user-edit',
+  templateUrl: './user-edit.component.html',
+  styleUrls: ['./user-edit.component.css']
+})
+export class UserEditComponent implements OnInit {
+
+  user: User = new User()
+  idUser: number
+  confirmarSenha: string
+  tipoUsuario: string
+
+  constructor(
+    private postagemService: PostagemService,
+    private authService: AuthService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+
+    window.scroll(0,0)
+
+    if(environment.token == '') {
+      //alert('Sua seção expirou, faça o login novamente.')
+      this.router.navigate(['/entrar'])
+    }
+
+    this.idUser = this.route.snapshot.params['id']
+    this.findByIdUser(this.idUser)
+
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value
+  }
+
+  atualizar() {
+    this.user.tipo = this.tipoUsuario
+
+    if(this.user.senha != this.confirmarSenha) {
+      alert('As senhas estão diferentes.')
+    }else {
+      this.authService.cadastrar(this.user).subscribe((resp: User) => {
+        this.user = resp
+        this.router.navigate(['/inicio'])
+        alert('Usuário atualizado com sucesso, faça o login novamente.')
+        environment.id= 0
+        environment.nome= ''
+        environment.foto= ''
+        environment.token= ''
+        this.router.navigate(['/login'])
+
+      })
+    }
+
+  }
+
+  findByIdUser(id: number) {
+    this.postagemService.getByIdUser(id).subscribe((resp: User) => {
+      this.user = resp
+    })
+
+  }
+
+}
